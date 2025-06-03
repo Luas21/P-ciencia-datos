d91e571d7f769cb9689fbc688158d32014ad2d1d
Luas21 <saul.garcia.lopez.2105@gmail.com>
2025-06-02 18:42:28 -0600

    prueba 8

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 #from config import config
 from routes import maquina, historico
 from models.maquinaModel import MaquinaModel
@@ -18,6 +18,41 @@ def index():
     except Exception as ex:
         return jsonify({'message': str(ex)}),500
 
+@app.route('/Historico')
+def get_historico():
+    try:
+        historico = 'hola'
+        return render_template('historico.html',historico=historico)
+    except Exception as ex:
+        return jsonify({'message': str(ex)}),500
+    
+@app.route('/api/graficos', methods=['POST'])
+def api_graficos():
+    try:
+        data = request.get_json()
+        if not data:
+            return jsonify({'error': 'JSON no recibido o inválido'}), 400
+
+        start_date = data.get('start_date')
+        end_date = data.get('end_date')
+
+        if not start_date or not end_date:
+            return jsonify({'error': 'Faltan fechas en el JSON'}), 400
+
+        resultado = MaquinaModel.get_historico_maquina(start_date, end_date)
+        return resultado  
+
+    except Exception as ex:
+        return jsonify({'error': str(ex)}), 500
+
+@app.route('/')
+def get_maquina(index):
+    try:
+        info_maquina = MaquinaModel.get_info_maquina(index) 
+        return render_template('detalle_maquina.html', maquina=info_maquina)
+    except Exception as ex:
+        return jsonify({'message': str(ex)}),500
+    
 
 def page_not_found(error):
     return "<h1>Nada por aquí...</h1>", 404
@@ -26,8 +61,8 @@ if __name__=='__main__':
     #app.config.from_object(config['development']) 
 
     #registro de otras rutas
-    app.register_blueprint(maquina.main, url_prefix='/detalle/<index>')
-    app.register_blueprint(historico.main, url_prefix='/Historico')
+    #app.register_blueprint(maquina.main, url_prefix='/detalle/<index>')
+    #app.register_blueprint(historico.main, url_prefix='/Historico')
 
     app.register_error_handler(404, page_not_found)
     app.run()
