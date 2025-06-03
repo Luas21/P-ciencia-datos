a59b823687bc1a4b0e3b4dbfbd69fb9b9ca6a3b6
Luas21 <saul.garcia.lopez.2105@gmail.com>
2025-06-02 18:28:03 -0600

    prueba 6

--- app/database/db.py
+++ app/database/db.py
@@ -1,16 +1,16 @@
 import psycopg2
 from psycopg2 import DatabaseError
-#from decouple import config
+from decouple import config
 
 #parámetros establecidos en .env para realizar la conexión a la BD
 def get_connection():
     try:
         return psycopg2.connect(
-            host="aws-0-us-east-2.pooler.supabase.com",
-            user="writer_user.upevccqurtfvymmgrhoa",
-            password="writer970",
-            database="postgres",
-            port=6543
+            host=config('DB_HOST'),
+            user=config('DB_USER'),
+            password=config('DB_PASSWORD'),
+            database=config('DB_NAME'),
+            port=config('DB_PORT')
         )
     except DatabaseError as ex:
         raise ex
